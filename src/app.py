f72521628ce93926d9aa74aca3d81d9613eedda7
jlfagundes-dev <jlfagundes@gmail.com>
2026-04-18 08:27:04 -0300

    feat: estrutura do app

---
+++ src/app.py
@@ -0,0 +1,24 @@
+# === Carregar os dados da pasta data que tem diferentes formatos ===
+
+# === Montar o contexto ===
+# Dados do cliente (Nome, Idade e Perfil)
+# Objetivo
+# Patrimônio
+# Transações Recentes
+# Atendimentos Anteriores
+# Produtos Disponiveis
+
+# === Criar o prompt ===
+# System Prompt
+
+# === Chamar o Gemini com apikey ===
+# Carregar system prompt
+# Carregar o contexto
+# Perguta do cliente
+# Gerar a resposta
+
+# === Gerar o frontend com Streamlit ===
+# sugestão não criar histórico de conversa, apenas pergunta a resposta atual
+
+
+
